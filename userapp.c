fa098c8181741481146bb20e45cf6009b33186aa
lall3 <saurav.lall@yahoo.co.in>
2018-02-20 15:11:10 -0600

    new

--- userapp.c
+++ userapp.c
@@ -1,63 +1,58 @@
 #include "userapp.h"
-#include <stdlib.h> 
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 
-void waste_time (unsigned long h)
-{
-	//if(h==0)
-	//return;
-	
-	long fact=1;
-	long i=0;
-	long m,n,b,c;
-	m=n=b=c=0;
-	while(i< h)
-	{
-		fact=fact*i;
-		i=i+2;
-		//i-=1;
-		while(m<h)
-		{
-			m++;
-			while(c<h)
-			{
-				c++;
-				n= m*c- n* i;
-			}
-		}
-	}
+void factorial (int);
+void call_kernel();
 
-}
+
 int main(int argc, char* argv[])
 {
-	unsigned long i=15000;
-	unsigned long j=0;
-int err;	
-FILE *proc_entry_fs; 
-char str[300];
-	//int pid= getpid();
-	//printf("PID IS %d", pid);	
-	while (j<i)
-	{
-		j++;
-		waste_time(j);
+  /*
+  printf("%d\n", (int) sizeof(argv));
+  if((int) sizeof(argv)<3){
+    printf("Usage: ./userapp repeat_time number\n"); 
+    exit(1);
+  }
+  */
+    
+	int time_factorial = atoi(argv[1]);
+	int value_factorial = atoi(argv[2]);
+
+	printf("\n=====I'm registering my process.\n");
+
+	pid_t mypid = getpid();
+	FILE *f = fopen ("/proc/mp1/status", "a");
+	int written_bytes = fprintf(f, "%d\0", mypid);
+	fclose(f);
+	printf("My pid is %i, pid has been registered\n", mypid);
+
+	printf("Factorial and call kernel starts, this test has %d rounds.\n", time_factorial);
+	while(time_factorial--!=0){
+		factorial(value_factorial);
+//		call_kernel();
 	}
-
-
-	proc_entry_fs = fopen("/proc/MP1/status", "w");
-	pid_t pid= getpid();
-	err=fprintf (proc_entry_fs, "%d",pid);
-	
-
-	char * msg = (char *) malloc (2048);
-
-	ssize_t read_bytes = read(fileno(proc_entry_fs), (void *) msg , 2048);
- 
-	printf("pid=%d, read: %s",pid,msg);
-	fclose(proc_entry_fs);
-
+	call_kernel();
 	return 0;
 }
+
+void factorial (int fact){
+
+	int val =1;
+	while(fact--!=0){
+		val *= fact ;
+	}
+}
+
+
+void call_kernel (){
+
+	printf("\n=====This is the calling kernel starts and read, update the time.\n");
+	FILE *f = fopen ("/proc/mp1/status", "r");
+	int size_buffer = 2048;
+	char * buffer = (char*)malloc(size_buffer);
+	ssize_t read_bytes = read (fileno(f), (void*)buffer, size_buffer);
+	buffer[read_bytes-1]='\0';
+	
+	printf("The read stuff is:\n%s", buffer); 		
+	fclose(f);
+}
+
